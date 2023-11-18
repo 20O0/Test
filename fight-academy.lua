8339c768beadc2fd0927d8e67dfb903a8568bffa
Alone <149536655+20O0@users.noreply.github.com>
2023-11-18 22:07:10 +0300

    Update fight-academy.lua

--- fight-academy.lua
+++ fight-academy.lua
@@ -6,27 +6,27 @@ local Window = Rayfield:CreateWindow({
    LoadingSubtitle = "Made by alone | DISCORD: g.0k ",
    ConfigurationSaving = {
       Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "Fighting Academy Hub"
    },
    Discord = {
       Enabled = true,
-      Invite = "ueXAyQFV7q", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Invite = "ueXAyQFV7q",
+      RememberJoins = true
    },
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "Key | Youtube Hub",
       Subtitle = "Key System",
       Note = "Key In Discord Server",
-      FileName = "YoutubeHubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/emsjm2uU"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      FileName = "YoutubeHubKey1",
+      SaveKey = true,
+      GrabKeyFromSite = true,
+      Key = {"https://pastebin.com/raw/emsjm2uU"}
    }
 })
 
-local MainTab = Window:CreateTab("🏠 Home", nil) -- Title, Image
+local MainTab = Window:CreateTab("🏠 Home", nil)
 local MainSection = MainTab:CreateSection("Main")
 
 Rayfield:Notify({
@@ -34,79 +34,74 @@ Rayfield:Notify({
    Content = "Very cool gui",
    Duration = 5,
    Image = 13047715178,
-   Actions = { -- Notification Buttons
+   Actions = {
       Ignore = {
          Name = "Okay!",
          Callback = function()
-         print("The user tapped Okay!")
-      end
+            print("The user tapped Okay!")
+         end
+      },
    },
-},
 })
 
 local Button = MainTab:CreateButton({
    Name = "Infinity Jump Toggle",
    Callback = function()
-       --Toggles the infinite jump between on or off on every script run
-_G.infinjump = not _G.infinjump
+      _G.infinjump = not _G.infinjump
 
-if _G.infinJumpStarted == nil then
-	--Ensures this only runs once to save resources
-	_G.infinJumpStarted = true
-	
-	--Notifies readiness
-	game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
+      if _G.infinJumpStarted == nil then
+         _G.infinJumpStarted = true
 
-	--The actual infinite jump
-	local plr = game:GetService('Players').LocalPlayer
-	local m = plr:GetMouse()
-	m.KeyDown:connect(function(k)
-		if _G.infinjump then
-			if k:byte() == 32 then
-			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
-			humanoid:ChangeState('Jumping')
-			wait()
-			humanoid:ChangeState('Seated')
-			end
-		end
-	end)
-end
+         game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
+
+         local plr = game:GetService('Players').LocalPlayer
+         local m = plr:GetMouse()
+         m.KeyDown:connect(function(k)
+            if _G.infinjump then
+               if k:byte() == 32 then
+                  humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+                  humanoid:ChangeState('Jumping')
+                  wait()
+                  humanoid:ChangeState('Seated')
+               end
+            end
+         end)
+      end
    end,
 })
-while wait(-1) do
-local Button = MainTab:CreateButton({
-   Name = "Auto Hit 'Only when combat item in equiped' ",
+
+local ButtonAutoHit = MainTab:CreateButton({
+   Name = "Auto Hit 'Only when combat item equipped'",
    Callback = function()
-   local args = {
-    [1] = game:GetService("Players").LocalPlayer
-}
+      local args = {
+         [1] = game:GetService("Players").LocalPlayer
+      }
 
-game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("kung-fu"):FireServer(unpack(args))
-end
+      game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("kung-fu"):FireServer(unpack(args))
    end,
 })
 
-local Slider = MainTab:CreateSlider({
+local SliderWalkSpeed = MainTab:CreateSlider({
    Name = "WalkSpeed Slider",
    Range = {1, 350},
    Increment = 1,
    Suffix = "Speed",
    CurrentValue = 16,
-   Flag = "sliderws", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "sliderws",
    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
    end,
 })
 
-local Slider = MainTab:CreateSlider({
+local SliderJumpPower = MainTab:CreateSlider({
    Name = "JumpPower Slider",
    Range = {1, 350},
    Increment = 1,
    Suffix = "Speed",
    CurrentValue = 16,
-   Flag = "sliderjp", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "sliderjp",
    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
+      game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
    end,
 })
 
@@ -115,9 +110,9 @@ local Dropdown = MainTab:CreateDropdown({
    Options = {"Starter World","Pirate Island","Pineapple Paradise"},
    CurrentOption = {"Starter World"},
    MultipleOptions = false,
-   Flag = "dropdownarea", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "dropdownarea",
    Callback = function(Option)
-        print(Option)
+      print(Option)
    end,
 })
 
@@ -126,49 +121,34 @@ local Input = MainTab:CreateInput({
    PlaceholderText = "1-500",
    RemoveTextAfterFocusLost = true,
    Callback = function(Text)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Text)
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(Text) or 16
    end,
 })
 
 local OtherSection = MainTab:CreateSection("Other")
 
---local Toggle = MainTab:CreateToggle({
---   Name = "Auto Farm",
---   CurrentValue = false,
---   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
---   Callback = function(Value)
---        print("FARMING")
--- end,
---})
-
-local TPTab = Window:CreateTab("🏝 Teleports", nil) -- Title, Image
+local TPTab = Window:CreateTab("🏝 Teleports", nil)
 
 local Button1 = TPTab:CreateButton({
    Name = "Karate boss",
    Callback = function()
-   
-x=601.6364135742188
-
-y=23.37848663330078
-
-z=-1114.469482421875
-
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
+      local x, y, z = 601.6364135742188, 23.37848663330078, -1114.469482421875
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x, y, z)
    end,
 })
 
 local Button2 = TPTab:CreateButton({
    Name = "Kung Fu Boss",
    Callback = function()
-        game.Players.LocalPlayer.Character.HumanidRootPart.CFrame = CFrame.new(601.6364135742188, 23.37848663330078, -1114.469482421875)
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(601.6364135742188, 23.37848663330078, -1114.469482421875)
    end,
 })
 
 local Button3 = TPTab:CreateButton({
    Name = "Equipment",
    Callback = function()
-        --Teleport3game.Players.LocalPlayer.Character.HumanidRootPart.CFrame = CFrame.new(-920.1085815429688, 9.057621955871582, -711.0045776367188)
+      -- Teleport3game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-920.1085815429688, 9.057621955871582, -711.0045776367188)
    end,
 })
 
-local TPTab = Window:CreateTab("🎲 Misc", nil) -- Title, Image
+local TPTabMisc = Window:CreateTab("🎲 Misc", nil)
